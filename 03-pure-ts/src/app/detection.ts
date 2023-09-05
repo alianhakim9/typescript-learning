8d8889a24a46a63958e0c4959d4b0c0cb663afc0
alianhakim9 <alianhakim9@gmail.com>
2023-09-05 12:26:13 +0700

    Instanceof and Type Predicates

--- 03-pure-ts/src/app/detection.ts
+++ 03-pure-ts/src/app/detection.ts
@@ -29,3 +29,32 @@ function isAdminAccount(account: User | Admin): boolean {
   }
   return false;
 }
+
+function logValue(value: Date | string) {
+  if (value instanceof Date) {
+    value.getDate();
+  } else {
+    value.toLowerCase();
+  }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+// function isFish(pet: Fish | Bird): boolean {
+//   return (pet as Fish).swim !== undefined;
+// }
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
